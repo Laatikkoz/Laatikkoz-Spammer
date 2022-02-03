c8f605d29863731639846b7979b1f382a71386b8
Laatikkoz <98652312+Laatikkoz@users.noreply.github.com>
2022-02-03 22:18:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import pyautogui, time
+
+print("""
+█░░ ▄▀█ ▄▀█ ▀█▀ █ █▄▀ █▄▀ █▀█   █▀ █▀█ ▄▀█ █▀▄▀█ █▀▄▀█ █▀▀ █▀█
+█▄▄ █▀█ █▀█ ░█░ █ █░█ █░█ █▄█   ▄█ █▀▀ █▀█ █░▀░█ █░▀░█ ██▄ █▀▄""")
+
+
+time.sleep(1.5)
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+clearConsole()
+
+teksti = input("Spam Text: ")
+
+
+input("Press Enter When You Are Ready")
+
+
+while True:
+    pyautogui.typewrite(teksti)
+    pyautogui.press("enter")
+
+
